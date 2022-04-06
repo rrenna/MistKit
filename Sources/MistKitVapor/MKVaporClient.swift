852144cf41dd82ca3908c3cb751ff6d26026cdb2
Ryan Renna <ryan.renna@mark43.com>
2022-04-06 17:08:14 -0400

    Added basic MKVaporServerToServerClient

--- Sources/MistKitVapor/MKVaporClient.swift
+++ Sources/MistKitVapor/MKVaporClient.swift
@@ -21,3 +21,27 @@ public struct MKVaporClient: MKHttpClient {
     return MKVaporClientRequest(client: client, request: clientRequest)
   }
 }
+
+public struct MKVaporServerToServerClient: MKHttpClient {
+  public let client: Client
+  public let headers:[String:String]?
+
+  public init(client: Client, headers: [String:String]? = nil) {
+    self.client = client
+    self.headers = headers
+  }
+
+  public func request(
+    fromConfiguration configuration: RequestConfiguration
+  ) -> MKVaporClientRequest {
+    var clientRequest = ClientRequest()
+    clientRequest.url = URI(string: configuration.url.absoluteString)
+    if let data = configuration.data {
+      clientRequest.body = ByteBuffer(data: data)
+      clientRequest.method = .POST
+      clientRequest.headers.add(name: .contentType, value: "application/json")
+      headers?.forEach({ clientRequest.headers.add(name: $0, value: $1) })
+    }
+    return MKVaporClientRequest(client: client, request: clientRequest)
+  }
+}
--- Sources/MistKitVapor/MKVaporClientRequest.swift
+++ Sources/MistKitVapor/MKVaporClientRequest.swift
@@ -5,12 +5,6 @@ public struct MKVaporClientRequest: MKHttpRequest {
   public let client: Client
   public let request: ClientRequest
 
-  public init(client: Client, request: ClientRequest)
-  {
-    self.client = client
-    self.request = request
-  }
-    
   public func execute(_ callback: @escaping ((Result<MKHttpResponse, Error>) -> Void)) {
     client.send(request).map(MKVaporClientResponse.init).whenComplete(callback)
   }
